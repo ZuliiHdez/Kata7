0805a51fe7d405b2fbdd8458e28bb5e15d6df8e3
Zuleima <zuleima2304@gmail.com>
2024-01-20 02:05:08 +0000

    Segunda Versión

--- src/main/java/software/ulpgc/kata7/Main.java
+++ src/main/java/software/ulpgc/kata7/Main.java
@@ -1,2 +1,16 @@
-package software.ulpgc.kata7;public class Main {
+package software.ulpgc.kata7;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new JFrame() {
+            {
+            this.setTitle("hello");
+            this.setSize(300,300);
+            this.setLocationRelativeTo(null);
+            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+             }
+        }.setVisible(true);
+    }
 }
